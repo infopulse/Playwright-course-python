c037d3aed51001e9f42d981b7222e5e91d31f291
ypurek <oleksii.ostapov@gmail.com>
2024-04-01 17:17:18 +0300

    added example with 2 contexts

---
+++ tests/test_example_06.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Browser
+
+
+def test_users(browser: Browser):
+    context1 = browser.new_context()
+    context2 = browser.new_context()
+    page1 = context1.new_page()
+    page2 = context2.new_page()
+    page1.goto("https://coffee-cart.app")
+    page2.goto("https://infopulse.com")
+    page1.pause()
+
+    page3 = context1.new_page()
+    page4 = context2.new_page()
+    page3.goto("https://playwright.dev")
+    page4.goto("https://python.org")
+    page1.pause()
