7c68b74dd48b74ee3bce24cb7334d55de0308f3c
ypurek <oleksii.ostapov@gmail.com>
2024-04-01 16:32:16 +0300

    make tests headed added recorded test

---
+++ tests/test_example_05.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://coffee-cart.app/")
+    page.locator("[data-test=\"Espresso\"]").click()
+    page.locator("[data-test=\"Espresso_Macchiato\"]").click()
+    page.locator("[data-test=\"Cappuccino\"]").click()
+    page.get_by_role("button", name="Yes, of course!").click()
+    expect(page.get_by_label("Cart page")).to_contain_text("cart (4)")
