d660adc818a7e553408ed5c82abe967564eaf7b6
ypurek <oleksii.ostapov@gmail.com>
2024-04-03 15:50:00 +0300

    added element selector examples

---
+++ tests/test_example_07.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Page, expect
+
+
+def test_get_method_example(page: Page):
+    page.goto('https://coffee-cart.app/')
+    espresso = page.get_by_label('Espresso', exact=True)
+    espresso.click()
+    expect(espresso).to_have_count(1)
+    expect(page.get_by_label("Cart page")).to_contain_text("cart (1)")
+
+
+def test_locator_features_1(page: Page):
+    page.goto('https://coffee-cart.app/')
+    whisky = page.get_by_label('Whisky', exact=True)
+
+    expect(whisky).not_to_be_visible()
+    assert whisky.is_visible() == False
+
+
+def test_locator_features_2(page: Page):
+    page.goto('https://coffee-cart.app/')
+    espresso = page.get_by_label('Espresso')
+    expect(espresso).to_have_count(3)
+
+
+def test_locator_features_3(page: Page):
+    page.goto('https://coffee-cart.app/')
+    espresso = page.get_by_label('Espresso')
+    espresso.first.click()
+    expect(page.get_by_label("Cart page")).to_contain_text("cart (1)")
---
+++ tests/test_example_08.py
@@ -0,0 +1,43 @@
+from playwright.sync_api import Page, expect
+
+
+def test_selectors(page: Page):
+    page.goto('https://coffee-cart.app/')
+    espresso = page.locator("css=[data-test=\"Espresso\"]")
+    capuccino = page.locator("[data-test=\"Cappuccino\"]")
+    americano = page.locator('//div[@aria-label="Americano"]')
+    expect(espresso).to_be_visible()
+    expect(capuccino).to_be_visible()
+    expect(americano).to_be_visible()
+
+    # BAD selector
+    # test = page.locator('//*[@id="app"]/div[2]/ul/li[6]/div/div/div[1]')
+    # expect(test).to_be_visible()
+
+
+def test_tips_1(page: Page):
+    page.goto('https://coffee-cart.app/')
+    li_items = page.locator('li')
+    syrup = page.locator('.syrup')
+    mocha = li_items.filter(has=syrup)
+    expect(mocha).to_contain_text('mocha', ignore_case=True)
+
+
+def test_tips_2(page: Page):
+    page.goto('https://coffee-cart.app/')
+    cafe = page.get_by_label('Cafe')
+    expect(cafe).to_have_count(2)
+    late = page.get_by_label('Cafe').and_(page.get_by_label('Latte'))
+    expect(late).to_have_count(1)
+
+    late_not_masala = page.get_by_label('Masala').or_(page.get_by_label('Latte'))
+    expect(late_not_masala).to_have_count(1)
+
+
+def test_tips_3(page: Page):
+    page.goto('https://coffee-cart.app/')
+    page.get_by_label('Espresso', exact=True).click()
+    page.goto('https://coffee-cart.app/', wait_until='networkidle')
+    page.wait_for_url('https://coffee-cart.app/', timeout=1000)
+    page.wait_for_selector('css=[data-test="Espresso"]', timeout=1000)
+    expect(page.get_by_label("Cart page")).to_contain_text("cart (0)")
