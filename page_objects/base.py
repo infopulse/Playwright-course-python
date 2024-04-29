18ebdf56625e0ddae465d452995d32d0856d54e6
ypurek <oleksii.ostapov@gmail.com>
2024-04-29 17:34:27 +0300

    events tests added

--- page_objects/base.py
+++ page_objects/base.py
@@ -1,12 +1,30 @@
-from playwright.sync_api import Page, Locator
+import logging
+from playwright.sync_api import Page, Locator, ConsoleMessage
+
+
+def get_log():
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
+    handler = logging.FileHandler('error.log')
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    return log
 
 
 class Base:
     def __init__(self, page: Page):
         self.page = page
+        self.log = get_log()
+
+        self.page.on('console', self._console_handler)
+
+    def _console_handler(self, msg: ConsoleMessage):
+        if msg.type.lower() in ('error', 'warning'):
+            self.log.error(f'{self.page.url}: {msg.type} {msg.text}')
 
     def goto(self, url, **kwargs):
-        self.page.goto(url, **kwargs)
+        self.page.goto(url, wait_until='networkidle', **kwargs)
 
     def navigate_to_menu(self) -> None:
         menu = self.page.locator('a[aria-label="Menu page"]')
---
+++ tests/test_example_13.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import expect
+
+
+def test_events_1(menu):
+    menu.goto('https://coffee-cart.app/?breakable=1')
+    menu.page.get_by_label('Espresso', exact=True).click()
+
+
+def test_events_2(menu):
+    context = menu.page.context
+    with context.expect_page() as page_info:
+        crt = menu.get_cart()
+        crt.click(button='middle')
+    page_2 = page_info.value
+    assert len(context.pages) == 2
+    page_2.close()
+    assert len(context.pages) == 1
+
+
+def test_events_3(menu):
+    nah_button = menu.page.get_by_text("Nah")
+
+    def handler():
+        nah_button.click()
+
+    menu.page.add_locator_handler(nah_button, handler)
+    for i in range(10):
+        menu.add_coffee('Espresso')
+    expect(menu.get_total()).to_contain_text("$100.00")
+
+
+def test_events_5(menu):
+    nah_button = menu.page.get_by_text("Nah")
+    menu.add_coffee('Espresso')
+    menu.add_coffee('Espresso')
+    menu.add_coffee('Espresso')
+    nah_button.wait_for(state='visible')
+    nah_button.click()
+    nah_button.wait_for(state='hidden')
+    expect(nah_button).not_to_be_visible()
