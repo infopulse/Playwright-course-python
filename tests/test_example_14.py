34e3b39f2ee92954516125a235d0563aea5a48c8
ypurek <oleksii.ostapov@gmail.com>
2024-04-30 13:38:27 +0300

    api tests

---
+++ tests/test_example_14.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import Page, expect
+
+
+def test_api(page: Page):
+    response = page.request.get('/list.json')
+    expect(response).to_be_ok()
+    assert len(response.json()) == 10
+
+    response = page.request.post('/list.json', data='hello world')
+    expect(response).not_to_be_ok()
+
+
+def test_coffee_14(menu):
+    menu.add_coffee("Espresso")
+    expect(menu.get_cart()).to_have_text("cart (1)")
+    expect(menu.get_total()).to_contain_text("$10.00")
+    response = menu.page.request.get('/list.json')
+    expect(response).to_be_ok()
---
+++ tests/test_example_15.py
@@ -0,0 +1,38 @@
+import os
+from pytest import mark, fixture
+from playwright.sync_api import Page, expect, BrowserContext, Browser
+
+config = 'config.json'
+
+
+@fixture(scope='session', autouse=True)
+def circus(browser: Browser):
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://circus.qamania.org/')
+    page.get_by_placeholder('Login').fill('a')
+    page.get_by_placeholder('Password').fill('a')
+    with page.expect_navigation(url='**', wait_until='networkidle'):
+        page.get_by_role('button', name='Login').click()
+    context.storage_state(path=config)
+    page.close()
+    yield context
+    if os.path.exists(config):
+        os.remove(config)
+
+
+def test_api(circus):
+    response = circus.request.get('http://circus.qamania.org/ws/performances')
+    expect(response).to_be_ok()
+
+
+def test_api_2(circus):
+    response = circus.request.get('http://circus.qamania.org/ws/performances')
+    expect(response).to_be_ok()
+
+
+@mark.browser_context_args(storage_state=config)
+def test_api_3(context: BrowserContext):
+    response = context.request.get('http://circus.qamania.org/ws/performances')
+    expect(response).to_be_ok()
